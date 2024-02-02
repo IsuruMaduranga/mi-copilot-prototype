c2321fde485520a778a0243d631997a78f46eddd
Isuru Wijesiri <isurumaduranga.official@gmail.com>
2024-02-02 16:26:39 +0530

    Add gunicorn

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-from typing import Deque, Literal
+from typing import List, Literal
 
 class Message(BaseModel):
     role: Literal['system','user', 'assistant']
     content: str
     
 class Chat(BaseModel):
-    chat_history: Deque[Message]
+    chat_history: List[Message]
